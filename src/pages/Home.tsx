debddacc1dbbac5aa9641f9d17c505dfd0474499
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-08-08 21:45:39 +0100

    feat: created home page with react router

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold h-screen flex items-center justify-center">Hello World!</h1>
+        </>
+    )
+}
+
+export default Home;
