4bc5f49312e7646ce8db7e6aae00ff7d0b272569
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-08-08 19:43:27 +0100

    feat: integrate Tailwind CSS and update project structure
    - Added Tailwind CSS as a dependency and configured it in vite.config.ts.
    - Removed unused assets and styles, including vite.svg, App.css, and react.svg.
    - Updated index.css to use Tailwind CSS for styling.
    - Simplified App component to display a "Hello world!" message using Tailwind CSS classes.
    - Added predeploy and deploy scripts for building and deploying the project to GitHub Pages.
    - Set homepage in package.json for proper deployment.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <h1 className="text-3xl font-bold h-screen flex items-center justify-center">Hello world!</h1>
   </StrictMode>,
 );
