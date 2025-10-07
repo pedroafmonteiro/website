fa2a1889005412f865ae2629352cdc75d45eb401
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-10-07 09:02:41 +0100

    feat: added resume button to website

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -98,23 +98,57 @@ const NavBar = () => {
               onClick={() => setMobileOpen(false)}
             />
           ))}
+
+          <hr className="w-1/2 border-t border-neutral-800" />
+
+          <li className="rounded-lg transition-all duration-200 p-2 hover:bg-neutral-800 cursor-pointer">
+            <a
+              href="/Pedro_Monteiro_Resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() => setMobileOpen(false)}
+              className="text-white select-none text-2xl md:text-base"
+            >
+              Resume
+            </a>
+          </li>
         </ul>
       </nav>
 
       {/* Larger screens */}
-      <nav
-        className={[
-          "hidden md:block",
-          "bg-neutral-900 border-2 border-neutral-800 shadow-sm shadow-neutral-800",
-          "w-min p-2 m-4 rounded-2xl",
-        ].join(" ")}
-      >
-        <ul className="flex flex-row justify-between space-x-2">
-          {navItems.map((item) => (
-            <NavItem key={item.label} label={item.label} route={item.route} />
-          ))}
-        </ul>
-      </nav>
+      <div className="flex flex-row">
+        <nav
+          className={[
+            "hidden md:block",
+            "bg-neutral-900 border-2 border-neutral-800 shadow-sm shadow-neutral-800",
+            "w-min p-2 m-4 rounded-2xl",
+          ].join(" ")}
+        >
+          <ul className="flex flex-row justify-between space-x-2">
+            {navItems.map((item) => (
+              <NavItem key={item.label} label={item.label} route={item.route} />
+            ))}
+          </ul>
+        </nav>
+        <div
+          className={[
+            "hidden md:block",
+            "bg-neutral-900 border-2 border-neutral-800 shadow-sm shadow-neutral-800",
+            "w-min p-2 my-4 rounded-2xl",
+          ].join(" ")}
+        >
+          <div className="rounded-lg transition-all duration-200 p-2 hover:bg-neutral-800 cursor-pointer">
+            <a
+              href="/Pedro_Monteiro_Resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white select-none text-2xl md:text-base"
+            >
+              Resume
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
