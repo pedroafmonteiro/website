55d68d2ef3c8d5e323a5f72563b8bc204c657277
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-12-23 18:58:05 +0000

    feat: updated information card to open links on a new page and added a color blur to the card

--- src/components/InformationCard.tsx
+++ src/components/InformationCard.tsx
@@ -7,16 +7,22 @@ const InformationCard = ({
   description,
   websiteLink,
   websiteLabel,
+  color,
 }: {
   title?: string;
   subtitle?: string;
   description?: string;
   websiteLink?: string;
   websiteLabel?: string;
+  color?: string;
 }) => {
   return (
-    <article className="p-4 rounded-2xl shadow-sm border-2 border-neutral-800 hover:border-white/20 bg-neutral-900 transition-all duration-150">
-      <header className="space-y-1 mb-4">
+    <article className="relative overflow-hidden p-4 rounded-2xl shadow-sm border-2 border-neutral-800 hover:border-white/20 bg-neutral-900 transition-all duration-150">
+      <div
+        className="absolute -top-10 -right-10 w-32 h-32 rounded-full blur-3xl opacity-30"
+        style={{ backgroundColor: color }}
+      />
+      <header className="relative space-y-1 mb-4">
         <h1 className="text-xl font-semibold text-neutral-200">{title}</h1>
         <p className="text-sm text-neutral-400">{subtitle}</p>
       </header>
@@ -26,6 +32,8 @@ const InformationCard = ({
       <footer className="flex justify-between gap-2">
         <a
           href={websiteLink}
+          target="_blank"
+          rel="noopener noreferrer"
           className="flex items-center text-neutral-200 space-x-2 hover:text-white transition-colors"
         >
           <GlobeIcon className="w-5 h-5" />
